4dc5ecf3f1e974da3aa9d3802f7a4f4bb81ca5a7
johnfitz3 <120284650+johnfitz3@users.noreply.github.com>
2023-04-02 17:51:03 -0400

    pushing to repo

---
+++ index.js
@@ -0,0 +1,122 @@
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require("path");
+
+const Engineer = require('./lib/engineer');
+const Intern = require('./lib/intern');
+const Manager = require('./lib/manager');
+
+
+const DIST_DIR = path.resolve(__dirname, "dist");
+const outputPath = path.join(DIST_DIR, "team.html");
+
+
+const generateHTML = require("./src/webpack.config");
+const { create } = require('domain');
+
+const employees = [];
+
+const addEmployee = () => {
+    return inquirer.prompt([
+      {
+        type: 'input',
+        name: 'employeeName',
+        message: 'Enter employee name'
+      },
+      {
+        type: 'input',
+        name: 'employeeId',
+        message: 'Enter employee ID'
+      },
+      {
+        type: 'input',
+        name: 'employeeEmail',
+        message: 'Enter employee Email',
+      },
+      {
+    type: 'list',
+    name: 'employeeRole',
+    message: 'Select employee role',
+    choices: ['Manager', 'Engineer', 'Intern']
+      },
+      {
+        type: 'input',
+        name: 'officeNumber',
+        message: 'Enter office number',
+        when: (answers) => answers.employeeRole === 'Manager'
+      },
+      {
+        type: 'input',
+        name: 'githubUser',
+        message: 'Enter github username',
+        when:(answers)=> answers.employeeRole === 'Engineer'
+      },
+      {
+        type:'input',
+        name: 'schoolName',
+        message: 'Enter School Name',
+        when: (answers) => answers.employeeRole === 'Intern'
+      },
+      {
+type: 'confirm',
+name: 'employeeAdd',
+message: 'Would you like to add an employee?'
+      },
+    ]).then((answers) => {
+       employees.push ({
+            name: answers.employeeName,
+            id: answers.employeeId,
+            email: answers.employeeEmail,
+            role: answers.employeeRole,
+            officeNumber: answers.officeNumber,
+            github: answers.githubUser,
+            school: answers.schoolName,
+            getName: function() {
+                return this.name;
+            },
+        });
+       if (answers.employeeAdd) {
+        return addEmployee();
+       } else {
+        return employees;
+       }
+    });
+  };
+
+
+
+  function createTeamFile() {
+    if (!fs.existsSync(DIST_DIR)) {
+        fs.mkdirSync(DIST_DIR);
+    } else {
+        fs.writeFileSync(outputPath, templateHTML(teamMembers), 'utf-8');
+        console.log('HTML file created in the dist folder');
+    }
+}
+
+
+function startApp() {
+    addEmployee().then((employees) => {
+        createTeamFile(employees);
+    });
+}
+
+startApp();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
